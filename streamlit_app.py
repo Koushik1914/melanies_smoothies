8882a6b1c27c97451ca6ffe50ad2b684cdba35df
Koushik1914 <164046533+Koushik1914@users.noreply.github.com>
2026-01-11 22:17:26 +0530

    Update Snowflake session handling in streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
+# -----------------------------
 # Import python packages
+# -----------------------------
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # -----------------------------
@@ -16,9 +17,10 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 # -----------------------------
-# Snowflake Session
+# Snowflake Connection (SniS)
 # -----------------------------
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # -----------------------------
 # Fruit Options
